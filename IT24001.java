ece49b956ef30da7b1029e78abb0110e11d95713
Swapnil Nag Swachha <snswachha@gmail.com>
2026-04-26 10:29:06 +0600

    Rename class from IT24061 to IT24001

--- IT24001.java
+++ IT24001.java
@@ -1,4 +1,4 @@
-public class IT24061 { //Create another class
+public class IT24001 { //Create another class
         public static void main(String[] args) {
                 Vehicle v = new Vehicle(args[0], args[1]);
                 v.start();
